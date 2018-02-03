33dbaf10245be687aaf482b6f2f5f4fb1ba7614a
Ricardo Beltrame <rickbelw@yahoo.com.br>
2018-02-03 14:07:10 -0200

    commit inicial

---
+++ src/main/java/br/com/onesolutionestetic/estetic/EsteticApplication.java
@@ -0,0 +1,12 @@
+package br.com.onesolutionestetic.estetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsteticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsteticApplication.class, args);
+	}
+}
